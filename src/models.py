a28306bc48342ce200ab2bf2aa2ab1312938ef90
sara-tustra <saraisantiagom@gmail.com>
2021-04-23 20:37:51 +0000

    Creando modelos de base de datos

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,54 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False, unique=True)
+    password = Column(String)
+    email = Column(String(250), nullable = False, unique = True)
+    first_name = Column(String(250), nullable = False)
+    last_name = Column(String(250))
+    posts = relationship('Post')
+    comments = relationship('Comment')
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    comments = relationship('Comment')
 
     def to_dict(self):
         return {}
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500), nullable = False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable = False)
+  
+
+    def to_dict(self):
+        return {}
+
+
+asociation_table = Table('asociation', Base.metadata,
+Column('user_from_id', Integer, ForeignKey('user.id')),
+Column('user_for_id', Integer, ForeignKey('user.id'))
+)
+
+   
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
